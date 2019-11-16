cb50c1d24e4348ee7b680f8eb2dd09c18aa39695
RuoChen95 <xrc_1995@163.com>
2019-11-16 21:09:02 +0800

    按需加载

--- src/main.js
+++ src/main.js
@@ -3,16 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import axios from 'axios';
-import echarts from 'echarts';
 
-// let echarts = require('echarts/lib/echarts');
-//
-// require('echarts/lib/chart/line');
-// require('echarts/lib/chart/bar');
-//
-// require('echarts/lib/component/title');
-// require('echarts/lib/component/tooltip');
-// require('echarts/lib/component/grid');
+let echarts = require('echarts/lib/echarts');
+
+require('echarts/lib/chart/line');
+require('echarts/lib/chart/bar');
+
+require('echarts/lib/component/title');
+require('echarts/lib/component/tooltip');
+require('echarts/lib/component/grid');
 
 Vue.prototype.$echarts = echarts;
 
